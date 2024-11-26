e9aa59bd81ec7462ddbbf9c400e41fe919832f3e
Youssef Elbatarwy <youssef.elbatrawy@student.guc.edu.eg>
2024-11-26 22:32:41 +0200

    finished reservation station structure

--- TomasuloAlgorithmSimulator/src/com/tomasulo/simulator/ReservationStation.java
+++ TomasuloAlgorithmSimulator/src/com/tomasulo/simulator/ReservationStation.java
@@ -1,5 +1,16 @@
 package com.tomasulo.simulator;
 
 public class ReservationStation {
-
+	String name;
+	boolean isBusy;
+	String op;
+	String v_j;
+	String v_k;
+	String q_j;
+	String q_k;
+	
+	public ReservationStation(String name) {
+		this.name = name;
+		this.isBusy = false;
+	}
 }
