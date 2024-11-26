3f12f6d8ae0b33d84ac9c7c25e543aafb0e2f2d0
Youssef Elbatarwy <youssef.elbatrawy@student.guc.edu.eg>
2024-11-26 21:10:14 +0200

    Adjusted Reg File Structure to include Q_i

--- TomasuloAlgorithmSimulator/src/com/tomasulo/simulator/RegisterFile.java
+++ TomasuloAlgorithmSimulator/src/com/tomasulo/simulator/RegisterFile.java
@@ -1,48 +1,62 @@
 package com.tomasulo.simulator;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 public class RegisterFile {
-	
-	HashMap<String, String> registerFile;
-	
-	public RegisterFile(String[] fps, String[] values) {
-        this.registerFile = IntStream.range(0, fps.length).collect(HashMap::new, (map, i) -> map.put(fps[i], values[i]), HashMap::putAll);
-	}
-	
-    public HashMap<String, String> getRegisterFile() {
+
+    private HashMap<String, String[]> registerFile;
+
+    public RegisterFile(String[] fps, String[] qI, String[] values) {
+        this.registerFile = new HashMap<>();
+
+        IntStream.range(0, fps.length).forEach(i -> {String qIValue = (qI != null && i < qI.length) ? qI[i] : null; String regValue = (values != null && i < values.length) ? values[i] : null; registerFile.put(fps[i], new String[]{qIValue, regValue});});
+    }
+
+    public Map<String, String[]> getRegisterFile() {
         return this.registerFile;
     }
-    
-    public String getFpValue(String target) {
+
+    public String[] getFpValue(String target) {
         if (!registerFile.containsKey(target)) {
             throw new IllegalArgumentException("Target key '" + target + "' does not exist in the register.");
         }
-
         return this.registerFile.get(target);
     }
-    
-    public void setFpValue(String target, String newValue) {
+
+    public void setFpValue(String target, String qI, String newValue) {
         if (!registerFile.containsKey(target)) {
             throw new IllegalArgumentException("Target key '" + target + "' does not exist in the register.");
         }
 
-        registerFile.put(target, newValue);
+        String[] existingEntry = registerFile.get(target);
+
+        String updatedQI = (qI != null) ? qI : existingEntry[0];
+        String updatedValue = (newValue != null) ? newValue : existingEntry[1];
+
+        registerFile.put(target, new String[]{updatedQI, updatedValue});
     }
-	
-    // Kont ba test hena
-//	public static void main(String[] args) {
-//		String[] a = {"f0", "f1", "f2", "f3"};
-//		String[] b = {"1", "2", "LD", "M1"};
-//		
-//		RegisterFile d = new RegisterFile(a, b);
-//		
-//		System.out.println(d.getRegisterFile());
-//		System.out.println(d.getFpValue("f0"));
-//		d.setFpValue("f0", "2");
-//		System.out.println(d.getFpValue("f0"));
-//		d.setFpValue("f10", "2");
-//		
-//	}
+
+    public static void main(String[] args) {
+        String[] fps =    {"f0", "f1", "f2", "f3"};
+        String[] qI =     {null, null, null, null}; 
+        String[] values = {null, null, null, "9"};
+
+        RegisterFile registerFile = new RegisterFile(fps, qI, values);
+
+        System.out.println("Before Update:");
+        registerFile.getRegisterFile().forEach((key, value) ->
+                System.out.println(key + " -> Q_i: " + value[0] + ", Value: " + value[1]));
+
+        registerFile.setFpValue("f1", "M1", null);
+
+        registerFile.setFpValue("f2", null, "35.0");
+
+        registerFile.setFpValue("f3", "M2", "50.0");
+
+        System.out.println("\nAfter Update:");
+        registerFile.getRegisterFile().forEach((key, value) ->
+                System.out.println(key + " -> Q_i: " + value[0] + ", Value: " + value[1]));
+    }
 }
