79584fa7899796d8d88aa7fbf1755f9b455ec09f
Youssef Elbatarwy <youssef.elbatrawy@student.guc.edu.eg>
2024-11-27 00:00:34 +0200

    finished pipeline structure

--- TomasuloAlgorithmSimulator/src/com/tomasulo/simulator/Pipeline.java
+++ TomasuloAlgorithmSimulator/src/com/tomasulo/simulator/Pipeline.java
@@ -1,5 +1,27 @@
 package com.tomasulo.simulator;
 
-public class Pipeline {
+import java.util.Queue;
+import java.util.LinkedList;
 
+public class Pipeline {
+	
+	Queue<Instruction> instructions;
+	RegisterFile registerFile;
+	LoadBuffer[] loadBuffers;
+	StoreBuffer[] storeBuffers;
+	ReservationStation[] adderReservationStations;
+	ReservationStation[] multiplierReservationStations;
+	
+	public Pipeline(Instruction[] instructions, String[] fps, String[] qI, String[] values, int loadBuffer, int storeBuffer, int adderReservationStations, int multiplierReservationStations) {
+		this.instructions = new LinkedList<>();
+		for(int i = 0; i < instructions.length; i++) {
+			this.instructions.add(instructions[i]);
+		}
+		
+		this.registerFile = new RegisterFile(fps, qI, values);
+		this.loadBuffers = new LoadBuffer[loadBuffer];
+		this.storeBuffers = new StoreBuffer[storeBuffer];
+		this.adderReservationStations = new ReservationStation[adderReservationStations];
+		this.multiplierReservationStations = new ReservationStation[multiplierReservationStations];
+	}
 }
